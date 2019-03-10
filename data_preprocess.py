c12c659b7dcf60f0d299749ee95747616a26059a
qwiksilva <rdsilva@stanford.edu>
2019-03-10 13:50:57 -0700

    added to data_preprocess

--- data_preprocess.py
+++ data_preprocess.py
@@ -8,14 +8,13 @@ import numpy as np
 original = pd.read_csv("data/warfarin.csv")
 dropna_cols = ['Age', 'Height (cm)', 'Weight (kg)',
           'Carbamazepine (Tegretol)', 'Phenytoin (Dilantin)',
-          'Rifampin or Rifampicin', 'Amiodarone (Cordarone)']
+          'Rifampin or Rifampicin', 'Amiodarone (Cordarone)', 'Therapeutic Dose of Warfarin']
 excluded_from_dropna = ['Race']
 
-#TODO:don't use dropna, keep all patents and set nan to 0
-df = original[dropna_cols + excluded_from_dropna]
-df.dropna(subset=dropna_cols, inplace=True)
-
-df['Therapeutic Dose of Warfarin'] = original['Therapeutic Dose of Warfarin'][df.index] / 7.0
+df = original.dropna(axis=0, subset=['Therapeutic Dose of Warfarin'])
+df = df[dropna_cols + excluded_from_dropna]
+# df.dropna(subset=dropna_cols, inplace=True)
+df['Therapeutic Dose of Warfarin'] = df['Therapeutic Dose of Warfarin'] / 7.0
 
 weight_of_race = {
     'White': 0,
@@ -25,16 +24,19 @@ weight_of_race = {
     'nan': .0443
 }
 
-df['Age'] = df['Age'].map(lambda a: int(a[0]))
+def map_age(a):
+    try: 
+        return int(a[0]) 
+    except: 
+        return a
+
+df['Age'] = df['Age'].map(map_age)
 
 for race in weight_of_race:
     df[race] = 0
 
-#TODO: Make sure nan is working here
 for index, row in df.iterrows():
     df[df['Race'][index]][index] = 1
-
-#TODO: normalize all features to mean 0 variance 1.
 
 def dosage_bucket(daily_dosage):
     if daily_dosage < 3:
@@ -47,7 +49,11 @@ def dosage_bucket(daily_dosage):
 Y = df['Therapeutic Dose of Warfarin'].map(dosage_bucket).values
 df = df.drop(columns=['Race', 'Therapeutic Dose of Warfarin'])
 
-print(df.head(5))
+#TODO: normalize features to mean 0 variance 1.
+normalize_features = df[['Age', 'Height (cm)', 'Weight (kg)']]
+df[['Age', 'Height (cm)', 'Weight (kg)']] = (normalize_features-normalize_features.mean())/normalize_features.std()
+
+df = df.fillna(0)
 
 X = df.values
 np.savez("features.npz", X_train=X, Y_train=Y)
